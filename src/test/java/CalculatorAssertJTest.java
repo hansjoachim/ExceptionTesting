7a990a97503a4af7acfb6710824ab804d1d202ed
Hans Joachim Desserud <github@desserud.org>
2015-05-05 20:48:43 +0200

    Add example using AssertJ to catch exceptions

---
+++ src/test/java/CalculatorAssertJTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class CalculatorAssertJTest {
+
+    @Test
+    public void should_throw_exception_when_adding_negative_numbers() throws Exception {
+        Calculator calc = new Calculator("AssertJ");
+
+        assertThatThrownBy(() -> calc.add(1, -2))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Adding a negative number makes no sense! Use subtract instead.");
+    }
+}
